620a919c26d36871b24613a6a62c958fe105e1a0
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-05-16 22:04:40 +0200

    Adding courses in the Work section

---
+++ src/components/Work/CourseCard.tsx
@@ -0,0 +1,90 @@
+import { Card, CardContent, Typography, CardHeader, IconButton, Chip, Stack, Button, Tooltip, Grid, Divider, FormControlLabel, Switch, Collapse } from "@mui/material";
+import IosShareOutlinedIcon from '@mui/icons-material/IosShareOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import SubCourse, { SubCourseType } from "./SubCourse";
+import { useState } from "react";
+
+interface CourseCardProps {
+    mobileView: boolean;
+    title: string;
+    status: string;
+    description?: string;
+    link?: string;
+    certificate?: string;
+    subcourses?: SubCourseType[];
+}
+
+export default function CourseCard(props: CourseCardProps) {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked((prev) => !prev);
+    };
+
+    return (
+        <Card sx={{ width: "100%", maxWidth: "90vw", background: 'rgba(0,0,0,0.7)', boxShadow: "none" }} >
+            <CardHeader
+                avatar={
+                    <LinkedInIcon color="secondary" style={{ fontSize: 35 }} />
+                }
+                action={props.link ?
+                    <IconButton color="primary" className="custom-link" href={props.link} target="_blank">
+                        <IosShareOutlinedIcon />
+                    </IconButton> : undefined
+                }
+            >
+            </CardHeader>
+            <CardContent>
+                <Stack direction="row" spacing={1}>
+                    <Typography variant="h5" sx={{ fontFamily: 'Nunito' }}>
+                        {props.title}
+                    </Typography>
+                    <Chip label={props.status} color="primary" variant="outlined" />
+                </Stack>
+                {props.certificate ?
+                    <Tooltip title="View Certificate">
+                        <Button sx={{ mt: 1.5, mb: 1.5 }} color="secondary" variant="outlined" target="_blank" href={props.certificate} startIcon={<WorkspacePremiumIcon />}>
+                            Certificate
+                        </Button>
+                    </Tooltip>
+                    : undefined}
+                <Typography variant="subtitle1">
+                    {props.description}
+                </Typography>
+
+                {props.subcourses ?
+                    <>
+                        <FormControlLabel
+                            style={{ marginTop: 15 }}
+                            control={<Switch sx={{
+                                '& .MuiSwitch-track': {
+                                    backgroundColor: '#D0D6C2'
+                                }
+                            }} checked={checked} onChange={handleChange} color="secondary" />}
+                            label={<Typography variant="body1" color="primary" sx={{ fontFamily: 'Monospace' }}>Show Subcourses</Typography>}
+                        />
+                        <Collapse in={checked}>
+                            <Stack mt={2.5} mb={1.5} direction="row" spacing={2} alignItems="center" justifyContent="flex-start">
+                                <Typography variant="h5" sx={{ fontFamily: 'Nunito' }}>
+                                    Subcourses
+                                </Typography>
+                                <Divider style={{ height: "1px", width: "70%", backgroundColor: "#D0D6C2" }} />
+                            </Stack>
+                            <Grid container direction={props.mobileView ? "column" : "row"} spacing={2} alignItems="flex-start">
+                                {props.subcourses?.map((subcourse) => (
+                                    <Grid item xs={12} md={6}>
+                                        <SubCourse
+                                            title={subcourse.title}
+                                            status={subcourse.status}
+                                            certificate={subcourse.certificate ? subcourse.certificate : undefined} />
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Collapse>
+                    </> : undefined}
+
+            </CardContent>
+        </Card>
+    );
+}
--- src/components/Work/WorkCardContent.tsx
+++ src/components/Work/WorkCardContent.tsx
@@ -1,20 +1,73 @@
-import { Collapse, Grid } from "@mui/material";
+import { Collapse, Divider, Grid, Stack, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import CardTitle from "../CardTitle";
+import CourseCard from "./CourseCard";
 import ProjectCard from "./ProjectCard"
 
-const items = [
+export enum CourseStatus {
+    IN_PROGRESS = 'In Progress',
+    COMPLETED = 'Completed'
+}
+
+const projects = [
     {
         title: 'Portfolio',
         description: 'Personal portfolio built with React Typescript.',
         link: 'https://github.com/chomelc/Portfolio',
-        technologies: ['React','Typescript', 'Material UI'],
+        technologies: ['React', 'Typescript', 'Material UI'],
     },
     {
         title: 'What ya eatin\'?',
         description: 'A simple webapp to organize and keep track of your weekly meals.',
         link: 'https://github.com/chomelc/WYE',
-        technologies: ['Python', 'Flask', 'React','Typescript'],
+        technologies: ['Python', 'Flask', 'React', 'Typescript'],
+    },
+];
+
+const courses = [
+    {
+        title: 'Become an Angular Developer',
+        status: CourseStatus.IN_PROGRESS,
+        description: `
+        Take your existing programming skills forward by learning how 
+        to build robust web apps using the Angular framework. Learn how 
+        to leverage the new component-based architecture to create efficient, 
+        cross-platform apps. See how to use structures, templates, 
+        change detection, data binding, and more.`,
+        link: 'https://www.linkedin.com/learning/paths/become-an-angular-developer-2',
+        certificate: 'test',
+        subcourses: [
+            {
+                title: 'Learning Angular',
+                status: CourseStatus.COMPLETED,
+                certificate: 'https://www.linkedin.com/learning/certificates/6d8f8af6e98b84d11511f0e3ea66cfa8053f63a8427fb5418535899dd2286c9b'
+            },
+            {
+                title: 'Angular: Ecosystems',
+                status: CourseStatus.COMPLETED,
+                certificate: 'https://www.linkedin.com/learning/certificates/a00ef72752d359712b18504256971370c66a397b544c9c285093b4a92d8310f5'
+            },
+            {
+                title: 'Angular Essential Training',
+                status: CourseStatus.COMPLETED,
+                certificate: 'https://www.linkedin.com/learning/certificates/c39964f7cf93487e452c1bf3f2ea99612f98dad382974d687014cd31e5134c8b'
+            },
+            {
+                title: 'Learning the Angular CLI',
+                status: CourseStatus.COMPLETED,
+                certificate: 'https://www.linkedin.com/learning/certificates/0b2d7febc7329d7c465bb53b3b64b02a525647cf1a03a09e90cb7d5251c32caa'
+            },
+            {
+                title: 'Angular: Testing and Debugging',
+                status: CourseStatus.IN_PROGRESS,
+                certificate: ''
+            },
+            {
+                title: 'Angular: API Communication and Authentication',
+                status: CourseStatus.IN_PROGRESS,
+                certificate: ''
+            },
+        ],
     },
 ];
 
@@ -45,15 +98,36 @@ export default function WorkCards() {
 
     return (
         <Collapse in={checked} {...(checked ? { timeout: 1000 } : {})}>
-            <CardTitle number="03." title="Some of my personal projects" />
-            <Grid container direction={mobileView ? "column" : "row"} spacing={2} alignItems="center" justifyContent="flex-start">
-                {items.map((item) => (
+            <CardTitle number="03." title="Some Things I've Done" />
+            <Grid container direction={mobileView ? "column" : "row"} spacing={2} alignItems="center" justifyContent="center">
+                {projects.map((project) => (
                     <Grid item>
                         <ProjectCard
-                            title={item.title}
-                            description={item.description}
-                            link={item.link}
-                            technologies={item.technologies} />
+                            title={project.title}
+                            description={project.description}
+                            link={project.link}
+                            technologies={project.technologies} />
+                    </Grid>
+                ))}
+                <Grid item xs={12}>
+                    <Stack mt={2.5} direction="row" spacing={2} alignItems="center" justifyContent="center">
+                        <Divider style={{ height: "1px", width: "40%", backgroundColor: "#FDB833" }} />
+                        <Typography variant="h5" color="primary" sx={{ fontFamily: 'Monospace' }}>
+                            Courses
+                        </Typography>
+                        <Divider style={{ height: "1px", width: "40%", backgroundColor: "#FDB833" }} />
+                    </Stack>
+                </Grid>
+                {courses.map((course) => (
+                    <Grid item xs={12} md={12}>
+                        <CourseCard
+                            mobileView={mobileView}
+                            title={course.title}
+                            status={course.status}
+                            description={course.description}
+                            link={course.link}
+                            certificate={course.certificate ? course.certificate : undefined}
+                            subcourses={course.subcourses ? course.subcourses : undefined} />
                     </Grid>
                 ))}
             </Grid>
